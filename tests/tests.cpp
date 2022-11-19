35af61d81e208c875647726ca3e3f4e433e6839b
sallyr <chenyan7@illinois.edu>
2022-11-18 20:00:34 -0600

    algo

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 TEST_CASE("file_reader working" ) {
     vector<vector<string>> airport = file_to_vector("/Users/sallyr/Documents/final_proj/dataset/airports.dat");
-    
-    REQUIRE("""Goroka Airport""" == airport[0][1]);
+    REQUIRE("Goroka Airport" == airport[0][1]);
 
 }
