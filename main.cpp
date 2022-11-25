07fc00bef36dbee018ea1fc5c818f5aae52bd94b
sallyr <chenyan7@illinois.edu>
2022-11-25 16:58:07 -0600

    coomit

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main()
     vector<string> out= airport[0];
     //unsigned ID, std::string name, std::string city,std::string country, std::string IATA, std::string ICAO,double latitude, double longitude
     Airport a(stoul(out[0]),out[1],out[2],out[3],out[4],out[5],stod(out[6]),stod(out[7]));
+    vector<vector<string >> edge = file_to_vector("data/airports.dat");
     Edge b;
     
     cout << airport.size() << endl;
